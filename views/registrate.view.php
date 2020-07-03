6a124ad100706b33a507b21ce547f604169e58f0
Julio Martinez <juliojwfuh@gmail.com>
2020-07-03 15:30:39 -0500

    registro

--- views/registrate.view.php
+++ views/registrate.view.php
@@ -12,6 +12,24 @@
 	<div class="contenedor">
 		<h1 class="titulo">Registrate</h1>
 		<hr class="border">
+	<form action="<?php echo htmlspecialchars( $_SERVER['PHO_SELF']);?>" method="POST" class="formulario" name="login">
+		<div class="form-group">
+			<i class="icono izquierda fa fa-user"></i><input type="text" class="usuario" name="usuario" placeholder="Usuario">
+		</div>
+		<div class="form-group">
+			<i class="icono izquierda fa fa-lock"></i><input type="password" class="password" name="password" placeholder="Contraseña">
+		</div>
+		<div class="form-group">
+			<i class="icono izquierda fa fa-lock"></i><input type="password" name="password2 " class="password_btn" placeholder="Repetir Conraseña">
+			<i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
+		</div>
+	</form>
 
+	<p class="texto-registrate">
+		¿ Ya tienes cuenta ?
+		<a href="login.php">Iniciar Secion</a>
+	</p>
+
+    </div>
 </body>
 </html>
